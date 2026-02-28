0f4ec22df3cc97b3de4aaaccb8b6ebeed0683cb5
mohamed <malyunomar10@gmail.com>
2026-03-01 00:45:33 +0300

    new lesson

--- you-suck-at-programming/foo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash 
-
-for thing in {1..12}; do
- 
-	echo "thing is $thing"
-done
--- you-suck-at-programming/lootest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-for name in munira abdi hassan; do
-	echo "this is my wife $name"
-done
