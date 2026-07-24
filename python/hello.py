2de49ed46c2184c23c427689b31101636d40e491
Mo farah <malyunomar10@gmail.com>
2026-07-24 08:16:39 -0500

    hello.py

--- python/hello.py
+++ python/hello.py
@@ -1,9 +1,24 @@
 
 #ask user for their name 
 
-name = input("what is your name: ")
+name = input("what is your name: ").strip().title()
 
+# Remove whitespaces from str and capitalize user's name 
+
+#name = name.strip().title()
+
+# capitalize user's name
+
+#name = name.capitalize()
+
+# split name into first and last name   
+
+first, last, = name.split(" ")
+
+# title user's name mean first latters 
+
+#name = name.title()
 
 # say hello to the user
 
-print(f"hello, {name}")
+print(f"hello, {first}")
