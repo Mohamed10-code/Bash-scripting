063c2f60533efb42ef2aa03f605c7fb90a4988ff
Mo farah <malyunomar10@gmail.com>
2026-07-27 11:13:40 -0500

    python

---
+++ python/compare.py
@@ -0,0 +1,13 @@
+
+
+x = int(input("what is x: "))
+y = int(input("what is y: "))
+
+
+if x < y:
+    print("x less than y")
+elif x > y:
+    print("x greater than y")
+
+else:
+    print("x is equal to y")
