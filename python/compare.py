16112a97c5164699a26a0c964d326459ea4e7206
Mo farah <malyunomar10@gmail.com>
2026-07-28 10:18:20 -0500

    python

--- python/compare.py
+++ python/compare.py
@@ -4,10 +4,14 @@ x = int(input("what is x: "))
 y = int(input("what is y: "))
 
 
-if x < y:
-    print("x less than y")
-elif x > y:
-    print("x greater than y")
+if x != y:
+    print("x is Not equal y")
 
 else:
     print("x is equal to y")
+
+#you can also use 
+# if x < y or x > y:
+#       print("x is not equal to Y")
+# else:
+    #print("x is equal to Y")
