463237b3386cf2413e18cbd6dbcc881254576acf
Mo farah <malyunomar10@gmail.com>
2026-07-29 15:28:02 -0500

    python

--- python/grade.py
+++ python/grade.py
@@ -1,13 +1,14 @@
 
-score = int(input("score: "))
 
-if score >= 90 and score <= 100:
-    print("grade: A")
-elif score >= 80 and score < 90:
-    print("grade: B")
-elif score >= 70 and score < 80:
-    print("grade: C")
-elif score >= 60 and score < 75:
-    print("grade: c")
+score = int(input("Score ?  "))
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
 else:
-    print("grade: F")
+    print("Grade: F")
