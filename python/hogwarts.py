73ffb26436107a9f6a2be9dd3380ba1b3d9d9f3a
Mo farah <malyunomar10@gmail.com>
2026-08-06 08:36:07 -0500

    python

---
+++ python/hogwarts.py
@@ -0,0 +1,12 @@
+
+students = {
+    "harry": "house-1",
+    "hermone": "house-2",
+    "farah": "house-3",
+    "ron": "house-4",
+
+}
+
+
+for student in students:
+    print(student, students[student], sep=" | ")
